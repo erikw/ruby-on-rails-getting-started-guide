2004cd491f79eea1db1c17b7bfe1084cc350e005
Erik Westrup <erik.westrup@gmail.com>
2021-10-29 13:04:09 +0200

    GET /items/:id

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,11 @@
 # frozen_string_literal: true
+
 class ItemsController < ApplicationController
   def index
     @items = Item.all
+  end
+
+  def show
+    @article = Item.find(params[:id])
   end
 end
