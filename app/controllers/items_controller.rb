8bcd3fb510eac30e8990cb62e65cc6afd2dfb3a4
Erik Westrup <erik.westrup@gmail.com>
2021-11-05 16:56:21 +0100

    Add Delete action for items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -37,6 +37,14 @@ class ItemsController < ApplicationController
     end
   end
 
+  def destroy
+    @item = Item.find(params[:id])
+    @item.destroy
+
+    # redirect_to root_path
+    redirect_to items_path
+  end
+
   private
 
   def item_params
